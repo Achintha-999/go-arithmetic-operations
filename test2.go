6b001df816059e0bf6c065d05e12fd2702acff1f
Achintha Sandaruwan <achinthasandaruwan517@gmail.com>
2025-11-21 14:52:32 +0530

    Initial commit: Added test2.go with basic arithmetic operations

---
+++ test2.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+	x := 10
+	y := 4
+
+	z := x + y
+
+	c := x - y
+
+	d := x * y
+
+	e := float32(x) / float32(y)
+	f := x % y
+
+	println(z, "\n")
+	println(c, "\n")
+	println(d, "\n")
+	println(e, "\n")
+	println(f, "\n")
+
+}
